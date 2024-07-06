7a72df29cc72607c1d5b78c1525217d2dec1debc
angelbastardo15 <angelbastardo77@gmail.com>
2024-07-06 10:41:51 -0400

    Variables y operadores matematicos

---
+++ Python_fundamentos/hola.py
@@ -0,0 +1,15 @@
+# asignación de variables
+number_1 = 22
+number_2 = 10
+
+# operaciones
+sum = number_1 + number_2
+substract = number_1 - number_2
+multi = number_1 * number_2
+div = number_1 / number_2
+
+# resultados
+print(sum)
+print(substract)
+print(multi)
+print(div)
